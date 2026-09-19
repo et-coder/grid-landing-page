4ca4037eedb90880627d08e782c854855af657da
Nati <thecodegeek1@gmail.com>
2026-09-19 14:16:27 +0300

    add image and navlinks to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,11 @@
 import menu from "../assets/icon-menu.svg";
+import hero from "../assets/hero.png";
+import arrow from "../assets/icon-arrow-right.svg";
+import close from "../assets/icon-close.svg";
+import plus from "../assets/icon-plus.svg";
+import sparkles from "../assets/icon-sparkle.svg";
+import trending from "../assets/icon-trending-up.svg";
 
-export { menu };
+const navLinks = ["About", "Our Work", "Partners", "Annual Report", "Donate"];
+
+export { menu, hero, arrow, close, plus, sparkles, trending, navLinks };
